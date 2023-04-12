c6c25f0c0969163070b3c809207d7f23504a02d2
Kawaeee <kawaekc@gmail.com>
2023-04-12 16:17:53 +0700

    🎉 initialize guragura

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Guragura</title>
+	<meta name="description" content="Just how I learn to Svelte!" />
+</svelte:head>
+
+<section>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+</style>
---
+++ src/routes/Footer.svelte
@@ -0,0 +1,23 @@
+<footer>
+    <p>© 2023 <a href="https://github.com/Kawaeee" target="_blank">Kawaeee</a>. All Rights Reserved.</p>
+</footer>
+
+<style>
+    footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>
