e8669bd4d82cb4edb32efe0b8111c4e64fe06296
Kawaeee <kawaekc@gmail.com>
2023-04-14 17:40:25 +0700

    🔨 update svelte.config.js to support github pages

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,8 @@ import adapter from '@sveltejs/adapter-static';
 import sveltePreprocess from "svelte-preprocess";
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     // Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -20,6 +22,9 @@ const config = {
         precompress: false,
         strict: true
     })
+    },
+    paths: {
+        base: dev ? '' : '/guragura',
     }
 };
 
